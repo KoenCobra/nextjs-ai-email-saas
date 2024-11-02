2acbdc66691ff2e930b3f259d7ee45d8c65781e1
Koen De Groot <koendegroot84@gmail.com>
2024-11-02 21:22:04 +0100

    added aurinko

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "~/components/ui/button";
+import { LinkAccountButton } from "~/components/link-account-button";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <LinkAccountButton />
     </div>
   );
 }
---
+++ src/components/link-account-button.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { getAurinkoAuthUrl } from "~/lib/aurinko";
+import { Button } from "./ui/button";
+
+export const LinkAccountButton = () => {
+  return (
+    <Button
+      onClick={async () => {
+        const authUrl = await getAurinkoAuthUrl("Google");
+        console.log(authUrl);
+      }}
+    >
+      Link account
+    </Button>
+  );
+};
